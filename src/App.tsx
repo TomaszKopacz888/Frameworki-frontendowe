78c1dd3b235d7e5d2b8716872a764048d5ae4531
t.kopacz <t.kopacz20@wp.pl>
2022-10-25 17:21:51 +0200

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import {FC} from 'react';
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
 import {Home} from './components/Routes/Home/home';
+import {Login} from './components/Routes/Login/login';
 import {Posts} from './components/Routes/Posts/posts';
+import {Cockpit} from './components/Routes/Cockpit/Cockpit';
 
 export const App: FC = () => {
     return (
@@ -12,10 +14,14 @@ export const App: FC = () => {
                 <>{console.log('test')}</>
                 <Link to="/">Home</Link>
                 <Link to='posts'>Posts</Link>
+                <Link to='login'>Login</Link>
+                <Link to='cockpit'>Cockpit</Link>
             </div>
             <Routes>
-                <Route path="posts" element={<Posts />}></Route>
                 <Route index element={<Home/>}></Route>
+                <Route path="posts" element={<Posts />}></Route>
+                <Route path="login" element={<Login />}></Route>
+                <Route path="cockpit" element={<Cockpit />}></Route>
             </Routes>
         </BrowserRouter>
     )
